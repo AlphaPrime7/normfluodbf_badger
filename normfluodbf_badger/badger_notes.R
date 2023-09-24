8abc48748762e5cb846bf66d6a8ec3a3d0cf295a
SwaggyT <98990077+AlphaPrime7@users.noreply.github.com>
2023-09-23 20:52:35 -0500

    Add files via upload

---
+++ normfluodbf_badger/badger_notes.R
@@ -0,0 +1,8 @@
+library(badger)
+badge_cran_download("normfluodbf", "grand-total", "yellow")
+badge_cran_download("normfluodbf", "last-week", "yellow")
+badge_cran_download("normfluodbf", "last-month", "green")
+build_readme()
+
+#A good JSON file to practice some dictionary manipulation in R
+#https://cranlogs.r-pkg.org/downloads/daily/last-month/normfluodbf
